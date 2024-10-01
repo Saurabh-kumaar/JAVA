403a8b85dc92ba9b0ec323ae6c4fb06c09ef9b30
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-10-01 07:00:56 +0530

    add

--- Bits.java
+++ Bits.java
@@ -1610,8 +1610,79 @@
 //                 }
 //         }
 // }
+// =================================================================== 
+// ===================================================================
+// ===================================================================
+// ===================================================================
+
+     
+// // **********  SORTING *************
+//      *************************
+//         *************
 
 
+//   import java.util.*; 
+
+// // Bubble sort 
+ 
+
+// public class Bits { 
+//         public static void printArray(int arr []){
+//                 for( int i=0; i<arr.length; i++){
+//                         System.out.print(arr[i] +" ");
+
+//                 }
+//                 System.out.println();
+//         }
+//         public static void main(String args []){
+//                 int arr [] = {9 , 4 , 8 , 2 , 1};
+                
+//                 //bubble sort
+//                 for ( int i =0; i<arr.length-1; i++){
+//                         for(int j=0; j<arr.length-i-1; j++){
+//                              if(arr[j] > arr[j+1]){
+
+//                                 //swap
+//                                 int tamp = arr[j];
+//                                 arr[j] = arr[j+1];
+//                                 arr[j+1] = tamp ;
+
+//                                 }
+//                         }
+//                 }
+//                 printArray(arr); 
+
+//         }
+// }
+
+
+// // Selection sort 
+
+
+// public class Bits { 
+//         public static void printArray(int arr []){
+//                 for( int i=0; i<arr.length; i++){
+//                         System.out.print(arr[i]+ " ");
+//                 }
+//                 System.out.println();
+//         }
+//         public static void main ( String args []){
+//                 int arr [] = { 5, 6, 2, 7, 1};
+
+//                 for(int i=0; i<arr.length-1; i++ ){
+//                         for(int j=0; j<arr.length-i-1; j++){
+//                                 if(arr[j] < arr[j+1]) {
+                                           
+                                       // //   swap 
+//                                         int tamp = arr[j];
+//                                         arr[j] = arr[j+1];
+//                                         arr[j+1] = tamp;
+//                                 }
+//                         }
+//                 } 
+//                 printArray(arr);
+//         }
+// }
 
 
  
