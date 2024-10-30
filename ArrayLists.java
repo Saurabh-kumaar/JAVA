37ca04c58fbe2cf2c9e958d18798381f43c4cacb
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-10-30 13:40:44 +0530

    Add

--- ArrayLists.java
+++ ArrayLists.java
@@ -2014,6 +2014,7 @@
 
 
 // import java.util.ArrayList;
+// import java.util.Collections;
 
 // class ArrayLists {
 //     public static void main (String args []){
@@ -2045,6 +2046,16 @@
  
 //          //size
 //          int size = list.size();
-//          System.out.println(size);
+//          System.out.println(size); 
+
+//         //  loops 
+//         for(int i=0; i<list.size(); i++){
+//             System.out.print(list.get(i));
+//         } 
+//         System.out.println(); 
+
+//         // sorting 
+//         Collections.sort(list);
+//         System.out.println(list);
 //     }
 // } 
