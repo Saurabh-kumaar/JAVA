5989aff72f6bf7c82d2d4dcddced11f399fe0286
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-10-30 09:50:06 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -1,17 +1,14 @@
-import java.util.*;
+// import java.util.Scanner;
 
-public class Practice {
-  public static void main(String args []){
-    Scanner sc = new Scanner(System.in);
+// public class Practice {
+//   public static void main(String args []){
+//     Scanner sc = new Scanner(System.in);
 
-      int a = sc.nextInt();
-      int b = sc.nextInt();
+//       int a = sc.nextInt();
+//       int b = sc.nextInt();
 
-      a = 7;
-      b = 9;
-      int sum = a+b;
-      
-      System.out.println(sum); 
-      sc.close();
-    }
-  }
+//       int sum = a + b;
+       
+//       System.out.println("the sum is =" + sum); 
+//     } 
+//   }  
