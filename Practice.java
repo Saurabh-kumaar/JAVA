e2d0a20f65402be786dd1c8f1d3c2f39904e0539
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-10-31 09:02:07 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -169,7 +169,12 @@
 //     public static void main (String args []){
 //         Scanner sc = new Scanner(System.in);
 //         String input = sc.nextLine();
-//         System.out.println(input);
+//         System.out.println(input); 
+
+//         int number = sc.nextInt();
+//         System.out.println(number); 
+
+//         float price = sc.nextFloat();
+//         System.out.println(price);
 //     }
 // }
-// ==================================================================== 
