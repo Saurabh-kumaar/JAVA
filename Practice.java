f28b1e88adbbadba856e9dd8f372819bb02003c8
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-10-31 08:56:48 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -161,3 +161,14 @@
 //     }
 // }
 // ================================================================= 
+
+// Input :- 
+
+// import java.util.*;
+// public class Practice {
+//     public static void main (String args []){
+//         Scanner sc = new Scanner(System.in);
+//         String input = sc.nextLine();
+//         System.out.println(input);
+//     }
+// }
