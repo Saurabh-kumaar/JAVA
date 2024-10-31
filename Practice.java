0700694ec3dd501e270b9695c2fd2b9fa8bff222
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-10-31 17:38:53 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -265,3 +265,18 @@
 // } 
 // ==================================================================== 
 
+// type Promotion in expression :-
+
+// import java.util.*; 
+
+// public class Practice {
+//     public static void main(String args []){
+//         Scanner sc = new Scanner(System.in);  
+//         char ch = 'a';
+//         char ch2 = 'b';
+//         int num = ch;
+//         int num2 = ch2;
+//         System.out.println(num);
+//         System.out.println(num2);
+//     }
+// }
