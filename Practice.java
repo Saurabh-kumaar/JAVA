c373c12d6a844500c0227cd49fec219882643512
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-10-31 17:44:47 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -282,15 +282,16 @@
 // } 
 // ========  
 // 2nd 
-import java.util.*; 
+// import java.util.*; 
 
-public class Practice {
-    public static void main(String args []){
-        int a = 10; 
-        float b = 20.25f;
-        long c = 25;
-        double d = 40;
-        double ans = a + b + c + d;
-        System.out.println(ans);
-    }
-}
+// public class Practice {
+//     public static void main(String args []){
+//         int a = 10; 
+//         float b = 20.25f;
+//         long c = 25;
+//         double d = 40;
+//         double ans = a + b + c + d;
+//         System.out.println(ans);
+//     }
+// }
+// ============================================================= 
