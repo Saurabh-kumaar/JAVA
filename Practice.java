cf3b6073f3e7b2d4d31234851f9c635e94608015
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-10-31 17:43:42 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -280,4 +280,17 @@
 //         System.out.println(num2);
 //     }
 // } 
-// ======== 
+// ========  
+// 2nd 
+import java.util.*; 
+
+public class Practice {
+    public static void main(String args []){
+        int a = 10; 
+        float b = 20.25f;
+        long c = 25;
+        double d = 40;
+        double ans = a + b + c + d;
+        System.out.println(ans);
+    }
+}
