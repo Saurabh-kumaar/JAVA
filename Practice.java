f96d54a9462d284d1ca8f1f7d515dfcc8b681b06
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-01 20:54:46 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -403,3 +403,18 @@
 //         System.out.println(b);
 //     }
 // }
+// ======== 
+
+// import java.util.*;
+// public class Practice{
+//     public static void main (String args []){
+//         int a = 3 ;
+//         int b = 6 ;
+//         System.out.println((a == b));
+//         System.out.println((a != b));
+//         System.out.println((a > b));
+//         System.out.println((a < b));
+//         System.out.println((a >= b )); 
+//         System.out.println((a <= b));
+//     }
+// }
