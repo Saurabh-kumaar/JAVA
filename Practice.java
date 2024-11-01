c2f38765a35a56740412280e05d28e2b527aad49
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-01 20:08:32 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -360,3 +360,18 @@
 //     }
 // }
 // ======================================================================== 
+
+// import java.util.*;
+// public class Practice{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         byte b = 4;
+//         char c = 'a';
+//         short s = 512;
+//         int i = 1000;
+//         float f = 3.14f;
+//         double d = 99.9954;
+//         double result = (f * d) +(i % c) - (d * s); 
+//         System.out.println(result);
+//     }
+// }
