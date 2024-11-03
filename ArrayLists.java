cde02185556a4c493ab46c6245013309be3f6729
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-03 07:45:36 +0530

    Add

--- ArrayLists.java
+++ ArrayLists.java
@@ -2025,7 +2025,7 @@
 //     public static void main(String args[]){
 //         int [] marks = new int[3];
 //         marks [0] = 78; // math 
-//         marks [1] = 98;
+//         marks [1] = 98; // chem
 //         marks [2] = 99; 
 //         //System.out.println(marks [0]); 
         
