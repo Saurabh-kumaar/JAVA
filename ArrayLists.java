362a35502f879d7626ad6af126542f5c6c8ea378
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-03 07:42:51 +0530

    Add

--- ArrayLists.java
+++ ArrayLists.java
@@ -2019,6 +2019,23 @@
 
 // //  ArrayLists Concept 
   
+// import java.util.*;
+
+// public class ArrayLists{
+//     public static void main(String args[]){
+//         int [] marks = new int[3];
+//         marks [0] = 78;
+//         marks [1] = 98;
+//         marks [2] = 99; 
+//         //System.out.println(marks [0]); 
+        
+//         for (int i = 0; i<3; i++){
+//             System.out.println(marks[i]);
+//         }
+//     }
+// }
+
+
 // import java.util.ArrayList;
 // import java.util.Collections;
 
