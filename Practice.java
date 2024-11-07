d8461ceffccd34fc3f38dcf126682cd8ef727154
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-07 20:48:48 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -1150,7 +1150,7 @@
 //     }
 // }
 // =======  
-            // reverse the given 
+            // reverse the given nu
 // class Practice {
 //     public static void main(String args []){
 //         int n = 235599;
