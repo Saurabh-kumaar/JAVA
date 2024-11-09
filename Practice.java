72ff99ce0212b0dca233723003000042238a4bfb
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-09 13:57:58 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -1207,3 +1207,20 @@
 //     }
 // }
 // ======= 
+                    // check 
+// import java.util.*;
+
+// public class Practice {
+//     public static void main(String args []) {
+//         Scanner sc = new Scanner(System.in);
+//         do{
+//             System.out.println("enter your number ");
+//             int n = sc.nextInt(); 
+
+//             if(n % 10 == 0) {
+//                 break ;
+//             } 
+//             System.out.println(n);
+//         } while (true);
+//     }
+// } 
