f00ba74d7dd2cd3d7d4d6476491da366cf1befb1
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-09 11:10:17 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -1143,4 +1143,24 @@
 //         }
 //     }
 // }
-// =======
+// // ======= 
+// import java.util.*; 
+
+// public class Practice {
+//     public static void main(String args []) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         // for loop 
+//         // for ( int i = 1; i<=; i++){
+//         //     System.out.println("****");
+//         // } 
+
+//         // while loop 
+//                 int i = 1;
+//         while ( i<=4){
+//             System.out.println("****");
+//             i++;
+//         }   
+//         // System.out.println("****"); 
+//     }
+// }
