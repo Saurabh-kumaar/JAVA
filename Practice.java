53c33d78fa39533a88e77cc1dd3e4f7c307356a0
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-11 21:16:02 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -1395,7 +1395,7 @@
 // public class Practice {
 //     public static void main(String args []) {  
           
-//         for(int i=1; i<=4; i++){ 
+//         for(int i=1; i<=10; i++){ 
 //             for (int j=1; j<=i; j++) {
 //                      System.out.print("*");
 //             } 
@@ -1403,5 +1403,60 @@
 //         }  
 //     }
 // }
-======== 
+// ========   
+                // inverted star pattern 
+// import java.util.*;
+
+// public class Practice {
+//     public static void main(String args []) {  
+          
+//         for(int i=1; i<=4; i++){ 
+//             for (int j=5; j>=i; j--) {
+//                      System.out.print("*");
+//             } 
+//                      System.out.println(" ");
+//         }  
+//     }
+// }
+// ========  
+                // print number pattern 
+
+// import java.util.*; 
+
+// public class Practice {
+
+//     public static void main(String args[]) {  
+//         Scanner sc = new Scanner(System.in);
+//         int n = 4; 
+
+//         for( int i=1; i<=n; i++) {
+//             for( int j = 1; j<=i; j++ ){
+//                 System.out.print(j);
+
+//             } 
+//             System.out.println();
+//         }   
+//     }
+// } 
+// ======== 
+                // alphabets pattern print 
+
+// import java.util.*; 
+
+// public class Practice {
+
+//     public static void main(String args[]) {  
+//         Scanner sc = new Scanner(System.in);
+//         int n = 5;
+//         char ch = 'A';
+        
+//         for(int line =1; line<=n; line++) {
+//             for(int chars=1; chars<=line; chars++){
+//                 System.out.print(ch + " ");
+//                 ch++;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
 
