e6175c85efb950f6310f6da41a76aaa468e2f23e
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-11 08:37:09 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -1389,4 +1389,19 @@
 // }
 // }
 // ======= 
+                    // star pattern 
+// import java.util.*;
+
+// public class Practice {
+//     public static void main(String args []) {  
+          
+//         for(int i=1; i<=4; i++){ 
+//             for (int j=1; j<=i; j++) {
+//                      System.out.print("*");
+//             } 
+//                      System.out.println(" ");
+//         }  
+//     }
+// }
+======== 
 
