1a0ebfe145a6e4a984c9af6d2d0f475d008da30b
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-11 07:20:36 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -1203,7 +1203,6 @@
 //             System.out.println(i);
 //         } 
 //         System.out.println("i am out of the loop ");
-
 //     }
 // }
 // ======= 
@@ -1225,3 +1224,97 @@
 //     }
 // }  
 // ======= 
+            // continue statement 
+// import java.util.*;
+
+// public class Practice {
+
+//     public static void main(String args []){
+//         for(int i=1; i<=5; i++) {
+//             if( i == 3){
+//                 continue;
+//             } 
+//             System.out.println(i);
+//         } 
+//         System.out.println("end now");
+//     }
+// }
+// ======  
+
+        // ques to continue keyboard 
+        
+// import java.util.*; 
+
+// public class Practice {
+//     public static void main(String args []){
+//         Scanner sc = new Scanner(System.in);
+//         do { 
+//         System.out.println("enter your number ");
+//         int n = sc.nextInt(); 
+//         if( n % 10 == 0 ) {
+//             continue;
+//         } 
+//         System.out.println(n);
+
+//         } while (true);
+//     }
+// }
+// ======= 
+                // check if a num is prime or not 
+
+// import java.util.*; 
+
+// public class Practice {
+//     public static void main(String args []) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+         
+//         if(n ==2 ){
+//             System.out.println("n is prime");
+//         } else {
+//             boolean isPrime = true;
+//             for (int i=2; i<=n-1; i++) {
+//                 if(n % i == 0) {
+//                     isPrime = false ;
+//                 }
+//             } 
+//             if(isPrime == true) {
+//                 System.out.println("n is prime");
+//             } else {
+//                 System.out.println("n is not prime");
+//             }
+//         }    
+//     }
+// } 
+// ======== 
+// how many times hello is printed . 
+
+// public class Practice {
+//     public static void main(String args []) {
+//         for(int i=0; i<5; i++){   // i=2 because i +=2 : 0+2 then i<5 
+//             System.out.println("hello");
+//             i +=2; 
+//         }
+//     }
+// } 
+// ========= 
+// import java.util.Scanner;
+// public class Practice {
+//     public static void main(String[]args) {
+//         Scanner sc = new Scanner(System.in); 
+//         int product = sc.nextInt();
+        
+//         switch (product)  {
+//             case 1 : System.out.println("java");
+//             break;
+
+//             case 2 : System.out.println("python");
+//             break ;
+
+//             case 3 : System.out.println("dsa");
+//             break;
+
+//             default : System.out.println("invalid num");
+//         }
+//         }
+//     }
