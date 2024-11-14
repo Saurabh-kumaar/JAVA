81e5c2014ca74590245e05f24e1cef4badd836f5
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-15 00:45:04 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -1855,11 +1855,21 @@
 //             System.out.println("its odd num");
 //         }
 //     }
-// }
+// } 
+                // check reverse 
+// import java.util.*; 
 
+// public class Practice {
 
- 
+//      public static void main(String args []){
+//         Scanner sc = new Scanner(System.in); 
+//         System.out.print("please enter digit = ");
+//             int n = sc.nextInt();
 
-
-
-
+//             while( n > 0 ) {
+//                 int lastDigit = n % 10;
+//                 System.out.print( lastDigit + " ");
+//                 n = n/10;
+//             }
+//         }
+//      }
