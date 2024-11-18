131f61ce288c3c7938e0a858bb346f4004837e80
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-19 01:37:27 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -2252,6 +2252,7 @@
 // ====================================================================
 // ====================================================================
 
+                            // largest number 
 // import java.util.*;
 
 // public class Practice {
@@ -2275,4 +2276,35 @@
 //     int numbers [] = {1, 2, 6, 4, 8};
 //     System.out.println("largest value = " + largest(numbers));
 // }
+// }
+// ===================================================================
+// ===================================================================
+
+// import java.util.*; 
+
+// public class Practice {
+
+//     public static int binarySearch(int numbers [], int key){
+//         int start = 0, end = numbers.length-1;
+
+//         while(start <= end ) {
+//             int mid = (start + end) / 2 ; 
+
+//             if(numbers[mid] == key){
+//                 return mid;
+//             } 
+//             if(numbers[mid] < key) {
+//                 start = mid+1;
+//             } else {
+//                 end = mid-1;
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String args []) {
+//         int numbers [] = { 2, 4, 6, 8, 10, 12, 14};
+//         int key = 6;
+//         System.out.println("index for key is : " + binarySearch(numbers, key));
+//     }
 // }
