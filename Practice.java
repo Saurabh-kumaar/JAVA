135d64949a98f9518fd9d43936393dad2ca26050
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-18 23:19:49 +0530

    Add

--- Practice.java
+++ Practice.java
@@ -2251,3 +2251,28 @@
 // }
 // ====================================================================
 // ====================================================================
+
+// import java.util.*;
+
+// public class Practice {
+//     public static int largest(int numbers []) {
+//         int largest = Integer.MIN_VALUE; 
+//         int smallest = Integer.MAX_VALUE;
+
+//         for(int i=0; i<numbers.length; i++){
+//             if(largest < numbers[i]) {
+//                 largest = numbers[i];
+//             } 
+//             if(smallest > numbers[i]) {
+//                 smallest = numbers[i];
+//             }
+//         }
+//         System.out.println("smallest  value is " + smallest);
+//         return largest ;
+//     }
+
+// public static void main(String args []){
+//     int numbers [] = {1, 2, 6, 4, 8};
+//     System.out.println("largest value = " + largest(numbers));
+// }
+// 
