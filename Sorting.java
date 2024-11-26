7b2b7373d041b10fd99784dc127418b7d59a52e2
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-26 12:58:19 +0530

    Add

---
+++ Sorting.java
@@ -0,0 +1,34 @@
+//  import java.util.*; 
+
+//  public class Sorting {
+    
+//     public static void bubbleSort(int arr[]){
+//         for(int i=0; i<arr.length-1; i++){
+//             for(int j=0; j<arr.length-1-i; j++){
+//                 if(arr[j]> arr[j+1]){ 
+
+//                     // swap 
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+
+//                 }
+//             }
+//         }
+//     } 
+
+//     public static void printArr(int arr[]){
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+
+//     }
+ 
+//     public static void main(String args []) {
+//         int arr[] = {3,1,5,8,2};
+
+//         bubbleSort(arr);
+//         printArr(arr);
+//     }
+//  }
