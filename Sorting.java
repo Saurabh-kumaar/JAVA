a1d8d5a8aa61bfe60e6c68e8e9430ef89ea6d033
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-27 01:33:23 +0530

    Add

--- Sorting.java
+++ Sorting.java
@@ -80,156 +80,59 @@
 // ========================================================================
 // ========================================================================
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// import java.util.*; 
+
+// public class Sorting { 
+ 
+//     // bubble sort code 
+
+//     public static void bubbleSort(int arr[]){
+//         // loop 
+//         for(int i=0; i<arr.length-1; i++){
+//             for(int j=0; j<arr.length-1-i; j++){
+//                 if(arr[j] > arr[j+1]){
+
+//                     // swap 
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                 }
+//             }
+//         }
+//     } 
+
+//     // selection sort code 
+
+//     public static void selectionSort(int arr[]){
+//         for(int i=0; i<arr.length-1; i++){
+//             int minPos = i;
+//             for(int j=i+1; j<arr.length; j++){
+//                 if(arr[minPos] > arr[j]){
+//                     minPos = j;
+//                 }
+//             } 
+//             // swap 
+
+//             int temp = arr[minPos];
+//             arr[minPos] = arr[i];
+//             arr[i]  = temp;
+
+//         }
+//     }
+
+//     // for printing 
+
+//     public static void printArr(int arr[]){
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String args []){
+//         int arr [] = {8,3,6,2,7}; 
+
+//         // bubbleSort(arr);
+//         selectionSort(arr);
+//         printArr(arr);
+//     }
+// }
