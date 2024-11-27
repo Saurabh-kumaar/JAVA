d496ab3f5e343473f151940b8d37a120bf401f6a
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-27 23:52:42 +0530

    Add

--- Sorting.java
+++ Sorting.java
@@ -113,6 +113,60 @@
 //   }
 // } 
 
+// =============================================================================
+// =============================================================================
+
+import java.util.*; 
+public class Sorting {
+
+    // bubble sort
+
+//     public static void bubbleSort(int arr []){
+//         for(int i=0; i<arr.length-1; i++){
+//             for(int j=0; j<arr.length-1-i; j++){
+//                 if(arr[j] < arr[j+1]){
+
+//             // swap 
+//             int temp = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = temp ;
+//             }
+//         }
+//     }
+// }
+        // selection sort 
+         
+//     public static void selectionSort(int arr[]){
+//         for(int i=0; i<arr.length-1; i++){
+//             int minPos = i;
+//             for(int j=0; j<arr.length; j++){
+//                 if(arr[minPos] > arr[j]){
+//                     minPos = j;
+//                 }
+//             }
+//             // swap 
+//             int temp = arr[minPos];
+//             arr[minPos] = arr[i];
+//             arr[i] = temp;
+//         }
+//     }
+
+//     public static void printArr(int arr[]){
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+
+//  public static void main(String args []) {
+//     int arr [] = { 3,6,2,1,8,7,4,5,3,1};
+//     selectionSort(arr);
+
+//     // bubbleSort(arr);
+    
+//     printArr(arr);
+//     }
+// }
 
 
 
@@ -252,6 +306,8 @@
 //         }
 //         System.out.println();
 //     }
+
+
 //     public static void main(String args []){
 //         int arr [] = {8,3,6,2,7}; 
 
