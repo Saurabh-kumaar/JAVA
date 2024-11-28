02319476053c970e73e5fad08517b748912e1662
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-28 20:43:58 +0530

    Add

--- Sorting.java
+++ Sorting.java
@@ -167,42 +167,42 @@
         // Counting Sort 
 
 
-import java.util.*; 
+// import java.util.*; 
 
-public class Sorting {
+// public class Sorting {
 
-    public static void countingSort(int arr[]){
-        int largest = Integer.MIN_VALUE;
-        for(int i=0; i<arr.length; i++){
-            largest = Math.max(largest, arr[i]);
-        }
+//     public static void countingSort(int arr[]){
+//         int largest = Integer.MIN_VALUE;
+//         for(int i=0; i<arr.length; i++){
+//             largest = Math.max(largest, arr[i]);
+//         }
 
-        int count[] = new int [largest+1];
-        for(int i=0; i<arr.length; i++){
-            count[arr[i]]++;
-        } 
+//         int count[] = new int [largest+1];
+//         for(int i=0; i<arr.length; i++){
+//             count[arr[i]]++;
+//         } 
 
-        // sorting 
-        int j=0; 
-        for(int i=0; i<count.length; i++){
-            while(count[i] > 0 ){
-                arr[j] = i;
-                j++; 
-                count[i]--;
-            }
-        }
-    }
+//         // sorting 
+//         int j=0; 
+//         for(int i=0; i<count.length; i++){
+//             while(count[i] > 0 ){
+//                 arr[j] = i;
+//                 j++; 
+//                 count[i]--;
+//             }
+//         }
+//     }
 
-    // printing array 
-    public static void printArray(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+//     // printing array 
+//     public static void printArray(int arr[]){
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
 
-    public static void main(String args []){
-        int arr [] = { 1, 3, 5, 7, 3,1,4,};
+//     public static void main(String args []){
+//         int arr [] = { 1, 3, 5, 7, 3,1,4,};
 
         // countingSort(arr);
         // printArray(arr);
