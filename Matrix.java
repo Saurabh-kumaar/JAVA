cb45053ea4a4f4254893204b3b7dfb45e324f76a
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-30 10:07:04 +0530

    Add

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,6 @@
+
+                        // create matrix by indexing 
+
 // import java.util.*;
 
 // public class Matrix {
@@ -28,6 +31,7 @@
 
 
 // ==================================================================================
+// ==================================================================================
 
 // import java.util.*;
 
@@ -37,3 +41,29 @@
 //         System.out.println("hello chhoti");
 //     }
 // }
+// ===================================================================================
+import java.util.*;
+public class Matrix {
+public static void main(String args []){
+    
+    int matrix [] [] = new int [5][4];
+    int n=matrix.length, m= matrix[0].length; 
+
+    Scanner sc = new Scanner(System.in );
+    for(int i=0; i<n; i++){
+        System.out.print("enter digit for new index ..................... ");
+        for(int j=0; j<m; j++){ 
+            System.out.print("Enter something for " + j + " Index : ");
+            
+            matrix [i][j] = sc.nextInt(); 
+        }
+    } 
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            System.out.print(matrix[i][j] + " ");  
+        }
+        System.out.println();
+    }
+}
+}
