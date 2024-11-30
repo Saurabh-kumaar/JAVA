6c1235116fb99589d345748bf975fd959e383dab
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-11-30 08:54:20 +0530

    Add

---
+++ Matrix.java
@@ -0,0 +1,39 @@
+// import java.util.*;
+
+// public class Matrix {
+
+//     public static void main(String args []){
+//         int matrix [][] = new int [3][3]; 
+//         int n = matrix.length,  m = matrix[0].length ; 
+        
+//         Scanner sc = new Scanner(System.in);
+//         // input 
+//         for(int i=0; i<n; i++){
+//             System.out.print("Entey Something for next Line ----------------------------------------");
+//             for(int j=0; j<m; j++){
+//                 System.out.print("Entey Something for " + j + " Index : ");
+//                 matrix [i][j] = sc.nextInt();
+//             }
+//         }
+
+//         // output 
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 System.out.print(matrix[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// ==================================================================================
+
+// import java.util.*;
+
+// public class Matrix {
+
+//     public static void main(String[] args) {
+//         System.out.println("hello chhoti");
+//     }
+// }
