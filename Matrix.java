b6d30243a9593ea5eef15da332c13184f95710a5
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-03 08:10:22 +0530

    Add

--- Matrix.java
+++ Matrix.java
@@ -189,30 +189,34 @@
 // =======================================================================================
 // ======================================================================================= 
 
-import java.util.*; 
+// ques:- 1
 
-public class Matrix { 
+// import java.util.*; 
 
-    // public static int search(int matrix[][] , int keys){
+// public class Matrix { 
 
-    public static void main(String args[]){
-        int array [][] = {{4,7,8},{8,8,7}};
+//     public static void main(String args[]){
+//         int array [][] = {{4,7,8},{8,8,7}};
 
-        int countof7 = 0; 
+//         int countof7 = 0; 
 
-        for(int i=0; i<array.length; i++){
-            for(int j=0; j<array[0].length; j++){
+//         for(int i=0; i<array.length; i++){
+//             for(int j=0; j<array[0].length; j++){
 
-                if(array[i][j] == 7){
-                countof7++;
-            }
-        }
-    }
+//                 if(array[i][j] == 7){
+//                 countof7++;
+//             }
+//         }
+//     }
             
-        System.out.println("count of 7 is : " + countof7);
-    }
-}
+//         System.out.println("count of 7 is : " + countof7);
+//     }
+// }
 
+// =======================================================================================
+// =======================================================================================
+
+// ques:- 2 
 
 
 
