ae4f4695810954692908efbfde45bb322887e30e
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-04 07:49:53 +0530

    Add

--- Sorting.java
+++ Sorting.java
@@ -316,118 +316,50 @@
 // ===============================================================================================
 // ===============================================================================================
 
+                    // selection 
 
-import java.util.*; 
+// import java.util.*; 
 
-public class Sorting { 
+// public class Sorting { 
 
-    public static int selectionSort(int arr[]){
-        int n = arr.length;
+//     public static int selectionSort(int arr[]){
+//         int n = arr.length;
 
-        for(int i=0; i<n-1; i++){
-            int minIndex = i;
-            for(int j=i+1; j<n; j++){
-                if(arr[j] > arr[minIndex]){
-                }
-            }
-               minIndex = j; 
-        
+//         for(int i=0; i<n-1; i++){
+//             int minIndex = i;
+//             for(int j=i+1; j<n; j++){
+//                 if(arr[j] > arr[minIndex]){
+//                      minIndex = j; 
 
-            int temp = arr[i];
-            arr[i] = arr[minIndex]; 
-            arr[minIndex] = temp; 
-        }
-    } 
+//                 }
+//             }
 
-    public static void printArr(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        } 
-    }
+                // swap 
+//             int temp = arr[i];
+//             arr[i] = arr[minIndex]; 
+//             arr[minIndex] = temp; 
+//         }
+//         return -1;
+//     } 
 
-    public static void main(String args []){
-        int arr[] = { 4,6,2,8,1}; 
-        // int n = arr.length; 
+//     public static void printArr(int arr[]){
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         } 
+//     }
 
-        selectionSort(arr);
-        printArr(arr);
-    }
-}
+//     public static void main(String args []){
+//         int arr[] = { 4,6,2,8,1}; 
+//         // int n = arr.length; 
 
+//         selectionSort(arr);
+//         printArr(arr);
+//     }
+// }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// =======================================================================
+// ========================================================================
 
 
 
