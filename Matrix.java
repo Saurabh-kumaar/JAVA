7eeede8206934cbedb4985ff444f7809b7b1153f
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-05 07:55:08 +0530

    Add

--- Matrix.java
+++ Matrix.java
@@ -257,26 +257,50 @@
 
 //  Live :- 
 
-// print one collom 
+                    // print one collom 
 
-public class Matrix {
+// public class Matrix {
 
-    public static void printSpiral(int [][] matrix){
+//     public static void printSpiral(int [][] matrix){
 
 
-    int x = 0; 
+//     int x = 3; 
 
-    int col =x; 
-    for(int row=0; row<matrix.length; row++){
-        System.out.println(matrix[row][col]);
+//     int col =x; 
+//     for(int row=0; row<matrix.length; row++){
+//         System.out.println(matrix[row][col]);
+//     }
+// }
+
+//     public static void main(String args[]){
+//         int arr [][] = { {1,6,11,14,21}, {2,7,12,15,25}, {3,8,13,16,22}, {4,9,17,19,24}, {5,10,20,18,23}};
+
+//         printSpiral(arr);
+
+//     }
+// } 
+
+// =============================================================================================
+// =============================================================================================
+
+                    // print row 
+
+public class Matrix { 
+
+    public static void printRow(int [][] matrix){
+
+        int x = 3; 
+        int row = x;
+
+        for(int col=0; col<matrix.length; col++){
+            System.out.println(matrix[row][col]);
+        }
     }
-}
 
-    public static void main(String args[]){
-        int arr [][] = { {1,6,11,14,21}, {2,7,12,15,25}, {3,8,13,16,22}, {4,9,17,19,24}, {5,10,20,18,23}};
+    public static void main(String args []){
+         int arr [][] = { {1,6,11,14,21}, {2,7,12,15,25}, {3,8,13,16,22}, {4,9,17,19,24}, {5,10,20,18,23}};
 
-        printSpiral(arr);
-
+            printRow(arr);
     }
 }
 
