7b9d3e5935f4cb8ecd1323e8391a11ac9f3ffd6f
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-06 15:24:04 +0530

    Add

--- newString.java
+++ newString.java
@@ -262,25 +262,25 @@
  
                 // find the lowercase 
 
-import java.util.*; 
+// import java.util.*; 
 
-public class newString {
+// public class newString {
 
-    public static void main(String args []){
+//     public static void main(String args []){
 
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        int count = 0;
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
+//         Scanner sc = new Scanner(System.in);
+//         String s = sc.nextLine();
+//         int count = 0;
+//         for(int i=0; i<s.length(); i++){
+//             char ch = s.charAt(i);
 
-            if(ch =='a' || ch =='e' || ch =='i' || ch =='o' || ch =='u'){
-                count++;
-            }
-        }
-        System.out.println("Lower Case vowels : " + count);
-    }
-}
+//             if(ch =='a' || ch =='e' || ch =='i' || ch =='o' || ch =='u'){
+//                 count++;
+//             }
+//         }
+//         System.out.println("Lower Case vowels : " + count);
+//     }
+// }
 
 
 
