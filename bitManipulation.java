2351cf0c3aaf3154d304dbac2590e41b87c84e03
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-08 00:52:29 +0530

    Add

--- bitManipulation.java
+++ bitManipulation.java
@@ -112,17 +112,17 @@
 // }
 
 
-import java.util.*; 
+// import java.util.*; 
 
-public class bitManipulation { 
+// public class bitManipulation { 
 
-    public static int setBit(int n, int i){
-        int bitMask = ~(1<<i);
-        return n & bitMask; 
-    }
+//     public static int setBit(int n, int i){
+//         int bitMask = ~(1<<i);
+//         return n & bitMask; 
+//     }
 
-    public static void main(String args []){ 
-        System.out.println(setBit(10, 1));
+//     public static void main(String args []){ 
+//         System.out.println(setBit(10, 1));
 
-    }
-}
+//     }
+// }
