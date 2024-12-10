05dcf0089d579d2e6b8595b4337d03286c13ae02
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-10 09:46:32 +0530

    Add

---
+++ OOPS.java
@@ -0,0 +1,58 @@
+// public class OOPS { 
+
+//     public static void main(String args[]){
+//         Pen p1 = new Pen();   // created a new 
+//         p1.setColor("Red");
+//         System.out.println(p1.getColor()); 
+
+//         p1.setTip (5); 
+//         System.out.println(p1.getTip());  
+
+//         p1.setColor("yellow");
+//         System.out.println(p1.getColor());
+
+//         // BankAccount myAcc = new BankAccount(); 
+//         // myAcc.username = "rajubhai";
+//         // myAcc.setPassword("ahdfui");
+//     }
+    
+// }
+
+
+//     // pass 
+
+// // class BankAccount {
+// //     public String username; 
+// //     private String password; 
+// //     public void setPassword(String pwd){
+// //         password = pwd;
+
+// //     }
+// // }
+
+// class Pen { 
+//     private String color; 
+//     private int tip; 
+//     String brand ; 
+
+//     // Getter .................................
+
+//     String getColor(){
+//         return this.color;
+//     } 
+
+//     int getTip (){
+//         return this.tip;
+//     } 
+
+//     // setter ...................................
+
+//     void setColor(String newColor){
+//         this.color = newColor; 
+//     } 
+
+//     void setTip (int newTip){
+//         this.tip = newTip ; 
+//     } 
+// }  
+ 
