80da31c259ce167871d239c136dbde074d38ab7d
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-11 12:38:44 +0530

    Add

--- OOPS.java
+++ OOPS.java
@@ -164,6 +164,58 @@
 //     }
 // }
 
+// public class OOPS {
+
+//     public static void main(String args []){ 
+//         Dog dobby = new Dog(); 
+//         dobby.eat(); 
+//         dobby.legs = 4; 
+//         System.out.println(dobby.legs);
+//     }
+// }
+
+// // Base class 
+// class Animal {
+//     String color; 
+
+//     void eat() {
+//         System.out.println("eats");
+//     } 
+
+//     void breathe(){
+//         System.out.println("breathes");
+//     }
+// }
+ 
+// // Derived class 
+
+// class Mammals extends Animal{
+//     int legs; 
+// }
+
+// class Dog extends Mammals {
+//     String breed; 
+// }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
