60428915d62f28c5d998342c91259761d528b362
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-15 12:00:35 +0530

    Add

--- OOPS.java
+++ OOPS.java
@@ -314,8 +314,38 @@
 // ============================================================================================= 
 
 
+// Static Keyword  :- 
 
+public class OOPS {
 
+    public static void main(String args []){
+      Student s1 = new Student(); 
+      s1.schoolName = "raju"; 
+      
+      Student s2 = new Student(); 
+      System.out.println(s2.schoolName); 
+
+      Student s3 = new Student(); 
+      s3.schoolName = "oxford";
+      System.out.println(s3.schoolName);
+    }
+}
+
+// -------- class 
+class Student { 
+    String name ; 
+    int roll; 
+
+    static String schoolName; 
+
+    void setName(String name){
+        this.name = name; 
+    }
+
+    String getName(){
+        return this.name; 
+    }
+}
 
 
 
