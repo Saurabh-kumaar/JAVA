e37c768d5eae0f564ce58fc0c6d6952c80f96281
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-16 15:14:57 +0530

    Add

--- OOPS.java
+++ OOPS.java
@@ -395,8 +395,7 @@ public class OOPS {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); 
 
-        // int n = 4;  
-        int sum = 0; 
+         int sum = 0; 
         for(int i=1; i<=n; i++){
             sum = sum + i; 
         } 
