9684da80579b75d9be7e88dab9bfbae3540feef7
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-17 20:18:03 +0530

    Add

--- Recursion.java
+++ Recursion.java
@@ -55,25 +55,46 @@
  
             // Find Factorial 
 
-            
+
+// public class Recursion { 
+
+//     public static int fact(int n){
+//         if(n == 0){
+//             return 1 ; 
+//         } 
+//         int fnm1 = fact(n-1);
+//         int fn = n * fact(n-1);
+//         return fn; 
+//     }
+
+//     public static void main (String args []){
+//         int n = 10; 
+//         System.out.println(fact(n));
+//     }
+// }
+
+
+// ==============================================================================================
+// ==============================================================================================
+
+        // print sum of first n naural number 
+
 public class Recursion { 
 
-    public static int fact(int n){
-        if(n == 0){
-            return 1 ; 
-        } 
-        int fnm1 = fact(n-1);
-        int fn = n * fact(n-1);
-        return fn; 
+    public static int printNatu(int n){  
+        if(n == 1){
+            return 1; 
+        }
+        printNatu(n-1);
+        System.out.println(n);
     }
 
-    public static void main (String args []){
-        int n = 10; 
-        System.out.println(fact(n));
-
+    public static void main(String args []){ 
+        int n  = 5; 
+        System.out.println(n);
+        printNatu(n);
     }
 }
-
 
 
 
