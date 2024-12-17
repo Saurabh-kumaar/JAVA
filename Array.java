b60d6b8bd87385584ead977be7aa81eb9b70d528
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-17 14:12:49 +0530

    Add

--- Array.java
+++ Array.java
@@ -2942,7 +2942,7 @@
 
 import java.util.*; 
 
-public class Practice {
+public class Array {
 
     public static void main(String args []){
         Scanner sc = new Scanner(System.in); 
---
+++ Recursion.java
@@ -0,0 +1,25 @@
+
+//             // RECURSION 
+
+
+
+//             // number in decreasing order 
+
+// public class Recursion { 
+
+//     public static void printDec(int n){
+//         if(n == 1){
+//             System.out.print(n);
+//             return; 
+//         } 
+//         System.out.print(n+ " ");
+//         printDec(n-1);
+//     }
+
+//     public static void main(String args []){
+//         int n = 12;  
+
+//         printDec(n);   // call kiya function ko 
+//     }
+// }
+ 
