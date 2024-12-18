de89dd4a2ecc9f1d88b00aaea574ff76bc797d9c
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-18 12:33:20 +0530

    Add

--- Recursion.java
+++ Recursion.java
@@ -150,8 +150,24 @@
 // ==================================================================================
 // ==================================================================================
 
+// public class Recursion {
 
+//     public static int findKey(int arr [],int key, int i){
+//         if(i == arr.length){
+//             return -1; 
+//         } 
+//         if(arr[i] == key){
+//             return i; 
+//         } 
+//         return findKey(arr, key , i+1);
+//     }
 
+//     public static void main (String args []){
+
+//     int arr [] = {3,4,1,8,5,9,7};
+//     System.out.println(findKey(arr,5,0));
+//     }
+// }
 
 
 
