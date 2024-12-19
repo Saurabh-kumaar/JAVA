7b97cec1f0ea6c0748b9011d4bac953cdba0ef70
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-19 23:37:49 +0530

    Add

--- Recursion.java
+++ Recursion.java
@@ -202,7 +202,26 @@
 // ================================================================================================
 // ================================================================================================
 
+public class Recursion { 
 
+    public static int tilingProblem(int n){   // 2 * n (florr size)
+
+        if(n == 0 || n == 1){
+            return 1; 
+        } 
+
+        int fnm1 = tilingProblem(n-1);
+
+        int fnm2 = tilingProblem(n-2);
+
+        int totalway = fnm1 + fnm2 ; 
+        return totalway; 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tilingProblem(4));
+    }
+}
 
 
 
