2cacc16ee607683b6d0e534c0ff4e0a2a30e4f4e
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-20 14:06:22 +0530

    Add

--- Recursion.java
+++ Recursion.java
@@ -263,30 +263,74 @@
 
 //   print binary string of size N without consecutive ones. 
 
+// public class Recursion { 
+
+//     public static void printBinstrings(int n, int lastPlace, String str){
+//         // base case 
+//         if(n == 0){
+//             System.out.println(str);
+//             return; 
+//         } 
+
+//         // kaam 
+//         printBinstrings(n-1,0,str+"0");
+//         if(lastPlace == 0){
+//             printBinstrings(n-1, 1, str+"1");
+//         }
+//        }
+
+//     public static void main(String args []){
+//         printBinstrings(3, 0, "");
+//     }
+// }
+
+// ==================================================================================
+// ==================================================================================
+
+
+// Solution :- 
+// ques 3 :- 
+
+// write a program to find  length of a string using recursion.  
+
+
+// public class Recursion { 
+
+//     public static int length(String str){
+//         if(str.length() == 0) {
+//             return 0; 
+//         } 
+//         return length(str.substring(1)) + 1; 
+//     }
+
+//     public static void main(String args []){
+//         String str = "hello raju"; 
+//         System.out.println(length(str));
+//     }
+// }
+
+// ==================================================================================
+// ==================================================================================
+ 
+                // tower of hanoi 
+                
 public class Recursion { 
 
-    public static void printBinstrings(int n, int lastPlace, String str){
-        // base case 
+    public static void toh(int n, int A, int C, int B){
         if(n == 0){
-            System.out.println(str);
             return; 
         } 
 
-        // kaam 
-        printBinstrings(n-1,0,str+"0");
-        if(lastPlace == 0){
-            printBinstrings(n-1, 1, str+"1");
-        }
-       }
+        toh(n-1, A, B, C);
+        System.out.println("moving " + n + " nth disk " + A + " to " + C );
+        toh(n-1, B, C, A);
+    }
 
-    public static void main(String args []){
-        printBinstrings(3, 0, "");
+    public static void main(String args []){ 
+        toh(3, 10, 30, 20); 
+        return;
     }
 }
-
-
-
-
 
 
 
