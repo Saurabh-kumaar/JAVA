d06b21180082bc0e2a0bd6a6268f6f44a90a2ea3
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-22 01:29:08 +0530

    Add

--- DivideConquer.java
+++ DivideConquer.java
@@ -73,55 +73,53 @@
 
             // QuickSort :- 
 
-public class DivideConquer {  
+// public class DivideConquer {  
 
-    // QuickSort 
-    public static void QuickSort(int arr[], int si, int ei){
-        if(si>= ei){
-            return;
-        }
-        int pIdx = partition(arr,si,ei);
-        QuickSort(arr,si,pIdx-1);
-        QuickSort(arr,pIdx+1, ei);
-    } 
+//     // QuickSort 
+//     public static void QuickSort(int arr[], int si, int ei){
+//         if(si>= ei){
+//             return;
+//         }
+//         int pIdx = partition(arr,si,ei);
+//         QuickSort(arr,si,pIdx-1);
+//         QuickSort(arr,pIdx+1, ei);
+//     } 
 
-    public static int partition(int arr[], int si, int ei){
-        int pivot = arr[ei];
-        int i = si-1;  
+//     public static int partition(int arr[], int si, int ei){
+//         int pivot = arr[ei];
+//         int i = si-1;  
 
-        for(int j=si; j<ei; j++){
-            if(arr[j] <= pivot){
-                i++; 
-                // swap 
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp ; 
-            }
-        }
-        i++; 
-        int temp = pivot; 
-        arr[ei] =  arr[i];
-        arr[i] =  temp ; 
-        return i;
-    }  
+//         for(int j=si; j<ei; j++){
+//             if(arr[j] <= pivot){
+//                 i++; 
+//                 // swap 
+//                 int temp = arr[j];
+//                 arr[j] = arr[i];
+//                 arr[i] = temp ; 
+//             }
+//         }
+//         i++; 
+//         int temp = pivot; 
+//         arr[ei] =  arr[i];
+//         arr[i] =  temp ; 
+//         return i;
+//     }  
 
-    // printing 
-    public static void printArray(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-    }
+//     // printing 
+//     public static void printArray(int arr[]){
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//     }
 
-    // main function 
-    public static void main(String args []){
-        int arr [] = { 6,3,9,8,2,5};
-        QuickSort(arr,0,arr.length-1);
-        printArray(arr);
-    }
-
-    
-}
+//     // main function 
+//     public static void main(String args []){
+//         int arr [] = { 6,3,9,8,2,5};
+//         QuickSort(arr,0,arr.length-1);
+//         printArray(arr);
+//     }
+// }
 
 
 
