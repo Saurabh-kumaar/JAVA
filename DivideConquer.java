3e128dd5581cec1b3d249ffef8401dc71049d2bc
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-22 02:27:14 +0530

    Add

--- DivideConquer.java
+++ DivideConquer.java
@@ -128,51 +128,93 @@
 
         // Rotated Sorted Array 
 
-public class DivideConquer { 
+// public class DivideConquer { 
 
-    public static int search(int arr[], int tar, int si, int ei){
-        if(si > ei){
-            return -1; 
-        }
+//     public static int search(int arr[], int tar, int si, int ei){
+//         if(si > ei){
+//             return -1; 
+//         }
         
-        //kaam 
-        int mid = si + (ei-si)/2; 
+//         //kaam 
+//         int mid = si + (ei-si)/2; 
 
-        // case found 
-        if(arr[mid] == tar){
-            return mid; 
-        } 
+//         // case found 
+//         if(arr[mid] == tar){
+//             return mid; 
+//         } 
 
-        // mid on L1 
-        if(arr[si] <= arr[mid]){
-            // case a : left 
-            if(arr[si] <= tar && tar <= arr[mid]){
-                return search(arr, tar, si, mid-1);
-            } else { 
-                // case b : right 
-                return search(arr, tar, mid+1, ei);
-            }
-        } 
+//         // mid on L1 
+//         if(arr[si] <= arr[mid]){
+//             // case a : left 
+//             if(arr[si] <= tar && tar <= arr[mid]){
+//                 return search(arr, tar, si, mid-1);
+//             } else { 
+//                 // case b : right 
+//                 return search(arr, tar, mid+1, ei);
+//             }
+//         } 
 
-        // mid on L2 
-        else { 
-            // case c : right 
-            if(arr[mid] <= tar && tar <= arr[ei]){
-                return search(arr, tar, mid+1, ei);
-            } else {
-                // case d : left 
-                return search(arr, tar, si, mid-1);
-            }
-        }
-    }
+//         // mid on L2 
+//         else { 
+//             // case c : right 
+//             if(arr[mid] <= tar && tar <= arr[ei]){
+//                 return search(arr, tar, mid+1, ei);
+//             } else {
+//                 // case d : left 
+//                 return search(arr, tar, si, mid-1);
+//             }
+//         }
+//     }
 
-    public static void main(String args []){
-        int arr[] = {4,5,6,7,3,0,1,2};
-        int target = 2; 
-        int tarIdx = search(arr,target,0,arr.length-1); 
-        System.out.println(tarIdx);
-    }
-}
+//     public static void main(String args []){
+//         int arr[] = {4,5,6,7,3,0,1,2};
+//         int target = 2; 
+        // int tarIdx = search(arr,target,0,arr.length-1); 
+        // System.out.println(tarIdx);
+        
+    // }
+// }
+
+
+// =========================================================================================================
+// =====================================================================s====================================
+
+
+// using one loop for sorted array
+
+// public class DivideConquer{
+
+//     public static void rotateByOne(int arr[]) {
+//         int n = arr.length;
+//         // last element
+//         int last = arr[n - 1];
+
+//          for (int i = n - 1; i > 0; i--) {
+//             arr[i] = arr[i - 1];
+//         }
+//         // Place the last element at the first position
+//         arr[0] = last;
+//     }
+
+//     public static void printArray(int arr[]) {
+//         for (int num : arr) {
+//             System.out.print(num + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String args[]) {
+//         int arr[] = {1, 2, 3, 4, 5};
+
+//         System.out.println("Original Array:");
+//         printArray(arr);
+
+//         rotateByOne(arr);
+
+//         System.out.println("Rotated Array:");
+//         printArray(arr);
+//     }
+// }
 
 
 
