c3de3f924064aba5e954a9914cdfc4f3e09d0038
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-26 23:26:52 +0530

    Add

--- BackTracking.java
+++ BackTracking.java
@@ -5,7 +5,7 @@
 //         if(i == arr.length){
 //             printArr(arr);
 //             return;
-//         }
+//         }                    time complexity = O(n);
 //         // resursion
 //         arr[i] = val;
 //         changeArr(arr, i+1, val+1);  //fnx call step 
@@ -21,13 +21,10 @@
  
 //     public static void main(String args []){
 //         int arr [] = new int[5];
-//         changeArr(arr, 0, 1);
+//         changeArr(arr, 0, 1); k
 //         printArr(arr); 
 //     }
 //  }
-
-
-
 
 
 
