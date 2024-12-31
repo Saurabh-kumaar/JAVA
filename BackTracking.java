0245c957ebc2231a57773b993e21efb5694f96db
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2024-12-31 14:09:24 +0530

    Add

--- BackTracking.java
+++ BackTracking.java
@@ -77,18 +77,23 @@
 //         for(int i=0; i<str.length(); i++){
 //             char curr = str.charAt(i);
 //             // "abcde" = "ab" + "de" = "abde"
-//             str = str.substring(0, i) + str.substring(i+1);
-//             findPermutation(str, ans+curr);
+//             String NewStr = str.substring(0, i) + str.substring(i+1);
+//             findPermutation(NewStr, ans+curr);
 //         }
 //     }
 
 //     public static void main(String args []){
-//         String str = "abc";
+//         String str = "123";
 //         findPermutation(str,"");
 //     }
 // }
 
 
+// ==================================================================================================================================
+// ==================================================================================================================================
+
+
+                // N queens 
 
 
 
