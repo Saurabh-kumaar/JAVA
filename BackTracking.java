c9e2e3dcd870433f7005ed0cbe5495686f2d4531
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-01-01 18:13:07 +0530

    Add

--- BackTracking.java
+++ BackTracking.java
@@ -171,7 +171,27 @@
 // // ==================================================================================================================================
 
 
+// public class BackTracking { 
 
+//     public static int gridways(int i, int j, int m, int n){
+
+//         // base case 
+//         if(i == n-1 && j == m-1){
+//             return 1;
+//         } else if(i == n || j == n) {
+//             return 0; 
+//         } 
+
+//         int w1 = gridways(i+1, j, m, n);
+//         int w2 = gridways(i, j+1, m, n); 
+//         return w1 + w2 ; 
+//     }
+
+//     public static void main(String args []){
+//         int n =4, m = 4; 
+//         System.out.println(gridways(0,0,n,m));
+//     }
+// }
 
 
 
