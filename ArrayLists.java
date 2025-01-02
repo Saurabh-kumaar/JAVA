d1bebe97f2b511a132280cb13edc214f1c93f77b
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-01-02 10:22:54 +0530

    Add

---
+++ ArrayLists.java
@@ -0,0 +1,51 @@
+
+//             // arraylists :- 
+
+
+// import java.util.*; 
+
+// public class ArrayLists { 
+
+//     public static void main(String args []){
+
+//         // className objective = new className(); 
+
+//         ArrayList<Integer> list = new ArrayList<>(); 
+//         ArrayList<String> list2 = new ArrayList<>();
+//         ArrayList<Boolean> list3 = new ArrayList<>();
+
+
+//         // Add Element 
+
+//         // list.add(1,5); set    // O(n) linear time 
+//         // System.out.println(list);
+
+
+//         list.add(1);        // 0(1) containst time
+//         list.add(2);
+//         list.add(3);
+//         list.add(4);
+
+//         System.out.println(list);
+  
+
+//         // Get Element 
+
+//         int element = list.get(1); 
+//         System.out.println(element); 
+
+//         // Remove element  O(n)
+//         list.remove(2);
+//         System.out.println(list);
+
+//         // set Elemnt index 
+//         list.set(2, 10);
+//         System.out.println(list);
+
+//         // contains 
+//         System.out.println(list.contains(1));
+//         System.out.println(list.contains(11)); 
+
+        
+//     }
+// }
