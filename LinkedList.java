8b83e0c5c884ca6cbcc7d50ef4afaf8caeefbcff
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-01-09 22:02:53 +0530

    Add

--- LinkedList.java
+++ LinkedList.java
@@ -303,7 +303,7 @@
 //         // System.out.println("ind is = " + ll.recSearch(5));
 
 //         // ll.reverse(); // reverse 
-//         // ll.print(); 
+        // ll.print(); 
 
 //         // ll.deleteNthfromEnd(4);  // deleteNth from end 
 //         // ll.print();
@@ -331,10 +331,62 @@
 
         // LinkedList next :------- series 2 
         
-        
+
+        // Detecting cycle code :- 
+
+
+import java.util.*; 
+
+public class LinkedList {
+
+    public static class Node{
+        int data; 
+        Node next; 
+
+        public Node(int data){
+            this.data = data; 
+            this.next = null; 
+        }
+    } 
+
+
+    
+    public static Node head; 
 
 
 
+
+
+
+
+
+
+
+    public static boolean isCycle() {
+        Node slow = head; 
+        Node fast = head; 
+
+        while(fast != null && fast.next != null){
+            slow = slow.next; 
+            fast = fast.next.next; 
+            if(slow == fast) {
+                return true;  // cycle detected
+            }
+        }
+        return false;  // no cycle
+    }
+
+    public static void main(String args []){ 
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head; 
+
+        System.out.println(isCycle());
+
+
+    }   
+}
 
 
 
