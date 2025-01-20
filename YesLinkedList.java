08dc9dbeb340836cd52158df7ff7dad8356d1eb8
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-01-20 09:45:23 +0530

    Add

--- YesLinkedList.java
+++ YesLinkedList.java
@@ -48,7 +48,7 @@
 //         if(head == null){
 //             head = tail = newNode;
 //             return;
-//         }ddd
+//         }
 //         tail.next = newNode; 
 //         tail = newNode; 
 
@@ -454,171 +454,171 @@
 
         // merge Sort on linked list :- 
 
- import java.util.*; 
+//  import java.util.*; 
 
- public class YesLinkedList { 
+//  public class YesLinkedList { 
 
-    public static class Node{
-        int data; 
-        Node next; 
+//     public static class Node{
+//         int data; 
+//         Node next; 
 
-        public Node(int data){
-            this.data = data; 
-            this.next = null; 
-        }     
-    }  
+//         public Node(int data){
+//             this.data = data; 
+//             this.next = null; 
+//         }     
+//     }  
 
-    public static Node head; 
+//     public static Node head; 
 
-    // addFirst 
-    public static void addFirst(int data){
-        Node newNode = new Node(data); 
-        if(head == null){
-            head = newNode; 
-            return; 
-        }
-        newNode.next = head; 
-        head = newNode;
-    }
+//     // addFirst 
+//     public static void addFirst(int data){
+//         Node newNode = new Node(data); 
+//         if(head == null){
+//             head = newNode; 
+//             return; 
+//         }
+//         newNode.next = head; 
+//         head = newNode;
+//     }
 
-    // print 
-    public static void print(){
-        Node temp = head; 
+//     // print 
+//     public static void print(){
+//         Node temp = head; 
 
-        while(temp != null){
-            System.out.print(temp.data + "->");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
+//         while(temp != null){
+//             System.out.print(temp.data + "->");
+//             temp = temp.next;
+//         }
+//         System.out.println("null");
+//     }
 
-    // getmid 
-    private Node getMid(Node head){
-        Node slow = head; 
-        Node fast = head.next; 
+//     // getmid 
+//     private Node getMid(Node head){
+//         Node slow = head; 
+//         Node fast = head.next; 
 
-        while(fast != null && fast.next != null){
-            slow = slow.next; 
-            fast = fast.next.next; 
-        }
-        return slow; // mid node
-    }
+//         while(fast != null && fast.next != null){
+//             slow = slow.next; 
+//             fast = fast.next.next; 
+//         }
+//         return slow; // mid node
+//     }
 
-    private Node merge(Node head1, Node head2){
-        Node mergedLL = new Node(-1);
-        Node temp = mergedLL; 
+//     private Node merge(Node head1, Node head2){
+//         Node mergedLL = new Node(-1);
+//         Node temp = mergedLL; 
 
-        while(head1 != null && head2 != null){
-            if(head1.data <= head2.data){
-                temp.next = head1; 
-                head1 = head1.next; 
-                temp = temp.next; 
-            } else {
-                temp.next = head2; 
-                head2 = head2.next; 
-                temp = temp.next; 
-            }
-        }
+//         while(head1 != null && head2 != null){
+//             if(head1.data <= head2.data){
+//                 temp.next = head1; 
+//                 head1 = head1.next; 
+//                 temp = temp.next; 
+//             } else {
+//                 temp.next = head2; 
+//                 head2 = head2.next; 
+//                 temp = temp.next; 
+//             }
+//         }
 
-        while(head1 != null){
-            temp.next = head1; 
-            head1 = head1.next; 
-            temp = temp.next; 
-        }
+//         while(head1 != null){
+//             temp.next = head1; 
+//             head1 = head1.next; 
+//             temp = temp.next; 
+//         }
 
-        while(head2 != null){
-            temp.next = head2;
-            head2 = head2.next; 
-            temp = temp.next;
-        }
-        return mergedLL.next;
-    }
+//         while(head2 != null){
+//             temp.next = head2;
+//             head2 = head2.next; 
+//             temp = temp.next;
+//         }
+//         return mergedLL.next;
+//     }
 
-    public Node mergeSort(Node head){
-        if(head == null || head.next == null){
-            return head; 
-        }
+//     public Node mergeSort(Node head){
+//         if(head == null || head.next == null){
+//             return head; 
+//         }
 
-        // find mid
-        Node mid = getMid(head); 
-        // left & right MS 
-        Node rightHead = mid.next; 
-        mid.next = null; 
-        Node newLeft = mergeSort(head);
-        Node newRight = mergeSort(rightHead);
+//         // find mid
+//         Node mid = getMid(head); 
+//         // left & right MS 
+//         Node rightHead = mid.next; 
+//         mid.next = null; 
+//         Node newLeft = mergeSort(head);
+//         Node newRight = mergeSort(rightHead);
 
-        // merge 
-        return merge(newLeft, newRight); 
-    }
+//         // merge 
+//         return merge(newLeft, newRight); 
+//     }
 
-    public void zigZag(){
-        // find mid 
-        Node slow = head; 
-        Node fast = head.next; 
-        while(fast != null && fast.next != null){
-            slow = slow.next; 
-            fast = fast.next.next; 
-        }
-        Node mid = slow; 
+//     public void zigZag(){
+//         // find mid 
+//         Node slow = head; 
+//         Node fast = head.next; 
+//         while(fast != null && fast.next != null){
+//             slow = slow.next; 
+//             fast = fast.next.next; 
+//         }
+//         Node mid = slow; 
 
-        // reverse 
-        Node curr = mid.next; 
-        mid.next = null; 
-        Node prev = null; 
-        Node next = null; 
+//         // reverse 
+//         Node curr = mid.next; 
+//         mid.next = null; 
+//         Node prev = null; 
+//         Node next = null; 
 
-        while(curr != null){
-            next = curr.next; 
-            curr.next = prev; 
-            prev = curr; 
-            curr = next; 
-        }
-        Node left = head; 
-        Node right = prev; 
-        Node nextL, nextR; 
+//         while(curr != null){
+//             next = curr.next; 
+//             curr.next = prev; 
+//             prev = curr; 
+//             curr = next; 
+//         }
+//         Node left = head; 
+//         Node right = prev; 
+//         Node nextL, nextR; 
 
-        // alt merge - zigZag merge 
-        while(left != null && right != null){
-            nextL = left.next; 
-            left.next = right; 
-            nextR = right.next; 
-            right.next = nextL; 
+//         // alt merge - zigZag merge 
+//         while(left != null && right != null){
+//             nextL = left.next; 
+//             left.next = right; 
+//             nextR = right.next; 
+//             right.next = nextL; 
 
-            left = nextL; 
-            right = nextR; 
-        }
-    }
-
-
-    public static void main(String args []){
-        YesLinkedList ll = new YesLinkedList();
-        // ll.addFirst(1);
-        // ll.addFirst(2);
-        // ll.addFirst(3);
-        // ll.addFirst(4);
-        // ll.addFirst(5);
-        // // 5->4->3->2->1  
-
-        // ll.print(); 
-        // ll.head = ll.mergeSort(ll.head);
-        //  ll.print(); 
-
-        // for zigZag 
-        ll.addFirst(5);
-        ll.addFirst(4);
-        ll.addFirst(3);
-        ll.addFirst(2);
-        ll.addFirst(1);
-        // 1->2->3->4->5 
-
-        ll.print();
-        ll.zigZag();
-        ll.print();
-
-    }
- }
+//             left = nextL; 
+//             right = nextR; 
+//         }
+//     }
 
 
+//     public static void main(String args []){
+//         YesLinkedList ll = new YesLinkedList();
+//         // ll.addFirst(1);
+//         // ll.addFirst(2);
+//         // ll.addFirst(3);
+//         // ll.addFirst(4);
+//         // ll.addFirst(5);
+//         // // 5->4->3->2->1  
+
+//         // ll.print(); 
+//         // ll.head = ll.mergeSort(ll.head);
+//         //  ll.print(); 
+
+//         // for zigZag 
+//         ll.addFirst(5);
+//         ll.addFirst(4);
+//         ll.addFirst(3);
+//         ll.addFirst(2);
+//         ll.addFirst(1);
+//         // 1->2->3->4->5 
+
+//         ll.print();
+//         ll.zigZag();
+//         ll.print();
+
+//     }
+//  }
+
+d
 
 
 
