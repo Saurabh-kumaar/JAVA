895af1fe2b618c13bc7e1e17d42075d94ec7dcf6
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-01-23 21:36:31 +0530

    Add

--- Stacks.java
+++ Stacks.java
@@ -224,50 +224,58 @@
 // ==================================================================================================== 
 // ==================================================================================================== 
 
-import java.util.*; 
 
-public class Stacks { 
+                    // Reverse a Stack 
 
-    public static void pushAtBottom(Stack<Integer> s, int data){
-        if(s.isEmpty()){
-            s.push(data);
-            return;
-        } 
 
-        int top = s.pop(); 
-        pushAtBottom(s, data);
-        s.push(top);
-    }
+// import java.util.*; 
 
-    public static void reverseStack(Stack<Integer> s){ 
-        if(s.isEmpty()){
-             return; 
-        }
+// public class Stacks { 
 
-        int top = s.pop();
-        reverseStack(s);
-        pushAtBottom(s, top);
-    }
+//     public static void pushAtBottom(Stack<Integer> s, int data){
+//         if(s.isEmpty()){
+//             s.push(data);
+//             return;
+//         } 
 
-    public static void printStack(Stack<Integer> s){
-        while(!s.isEmpty()){
-            System.out.println(s.pop());
-        }
-    }
+//         int top = s.pop(); 
+//         pushAtBottom(s, data);
+//         s.push(top);
+//     }
 
-    public static void main(String args []){
-        Stack<Integer> s = new Stack<>(); 
-        s.push(2); 
-        s.push(3);
-        s.push(4);  
+//     public static void reverseStack(Stack<Integer> s){ 
+//         if(s.isEmpty()){
+//              return; 
+//         }
 
-        printStack(s);
-        reverseStack(s);
+//         int top = s.pop();
+//         reverseStack(s);
+//         pushAtBottom(s, top);
+//     }
 
-    }
-}
+//     public static void printStack(Stack<Integer> s){
+//         while(!s.isEmpty()){
+//             System.out.println(s.pop());
+//         }
+//     }
 
- 
+//     public static void main(String args []){
+//         Stack<Integer> s = new Stack<>(); 
+//         s.push(2); 
+//         s.push(3);
+//         s.push(4);  
+
+//         printStack(s);
+//         reverseStack(s);
+
+//     }
+// }
+
+// ==================================================================================================== 
+// ==================================================================================================== 
+
+
+
 
 
 
