864829a03ce16d399f0f16ca97ec2fa96c471dfe
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-10 15:58:11 +0530

    Add

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -223,7 +223,7 @@ public class BinaryTreesB {
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-        root.right.left = new Node(6);
+        root.right.left = new Node(6);sd
         root.right.right = new Node(7); 
 
         System.out.println(height(root));
