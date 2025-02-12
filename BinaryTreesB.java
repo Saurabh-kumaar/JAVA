7287564beee2a53cf85db1fed66f8a1ef5d6264a
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-12 09:52:33 +0530

    Add

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -186,6 +186,9 @@
 
             // Height of a tree :- 
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BinaryTreesB {
     static class Node {
@@ -290,7 +293,56 @@ public class BinaryTreesB {
              }
         }
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    } 
 
+    static class Info {
+        Node node; 
+        int hd; 
+
+        public Info(Node node, int hd) {
+            this.node = node; 
+            this.hd = hd; 
+        }
+    } 
+
+    public static void topView(Node root) {
+        //level order 
+        Queue<Info> q = new LinkedList<>(); 
+        HashMap<Integer, Node> map = new HashMap<>(); 
+        
+        int min = 0, max = 0; 
+        q.add(new Info(root, 0)); 
+        q.add(null); 
+
+        while(!q.isEmpty()) {
+            Info curr = q.remove(); 
+            if(curr == null) {
+                if(q.isEmpty()) {
+                    break; 
+                } else {
+                    q.add(null); 
+                }
+            } else { 
+                if(!map.containsKey(curr.hd)) {
+                    map.put(curr.hd, curr.node);
+                }
+    
+                if(curr.node.left != null) {
+                    q.add(new Info(curr.node.left, curr.hd-1));
+                    min = Math.min(min, curr.hd-1); 
+                }
+    
+                if(curr.node.right != null) {
+                    q.add(new Info(curr.node.right, curr.hd+1));
+                    max = Math.max(max, curr.hd+1);
+                }
+            }   
+        }
+
+        for(int i=min; i<max; i++) {
+            System.out.print(map.get(i).data+" ");
+        }
+        System.out.println();
     }
 
     public static void main(String args []){ 
@@ -327,8 +379,8 @@ public class BinaryTreesB {
         // System.out.println("total sum is = " + sum(root));
         // System.out.println(diameter(root)); 
 
-        System.out.println(isSubtree(root, subRoot));
-
+        // System.out.println(isSubtree(root, subRoot)); 
+        topView(root);
         
     }
 }
