59ec05bb1d1a35f862f4845a0e2d3abd4bbdc487
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-13 23:35:38 +0530

    Add

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -528,6 +528,9 @@
 // ======================================================================================================== 
 
 
+        // Lowest common ancestor :- 
+
+
 
 import java.util.*; 
 
@@ -566,6 +569,7 @@ public class BinaryTreesB {
             return false; 
         }
 
+        // lca 
         public static Node lca(Node root, int n1, int n2) {
             ArrayList<Node> path1 = new ArrayList<>(); 
             ArrayList<Node> path2 = new ArrayList<>(); 
@@ -587,6 +591,12 @@ public class BinaryTreesB {
         }
     }
 
+
+// ==========================================================================================
+// ==========================================================================================
+
+    // approach 2 :-  lowest common ancestor 
+    
     public static Node lca2(Node root, int n1, int n2) {
         if(root.data == n1 || root.data == n2) {
             return root; 
@@ -604,8 +614,37 @@ public class BinaryTreesB {
         return root; 
     }
 
+    // ==========================================================================================
+    // ==========================================================================================
 
+    public static int lcaDist(Node root, int n) {
+        if(root == null) {
+            return -1; 
+        } 
 
+        if(root.data == n) {
+            return 0; 
+        }
+
+        int leftDist = lcaDist(root.left, n); 
+        int rightDist = lcaDist(root.right, n); 
+
+        if(leftDist == -1 && rightDist == -1) {
+            return -1; 
+        } else if (leftDist == -1) {
+            return rightDist+1; 
+        } else {
+            return leftDist+1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2); 
+        int dist1 = lcaDist(root, n1); 
+        int dist2 = lcaDist(root, n2); 
+
+        return dist1 + dist2; 
+    }
 
     public static void main(String args []) { 
 
@@ -624,10 +663,11 @@ public class BinaryTreesB {
 // //               / \ / \
 // //              4  5 6  7
 
-// //         */ 
+// //           */ 
 
                 int n1 = 4, n2 = 6; 
-                System.out.println(Node.lca(root, n1, n2).data); 
+                // System.out.println(Node.lca(root, n1, n2).data);  
+                System.out.println(minDist(root, n1, n2));
 
 
     }
@@ -636,22 +676,11 @@ public class BinaryTreesB {
 
 
 
+// ==========================================================================================
+// ==========================================================================================
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
 
