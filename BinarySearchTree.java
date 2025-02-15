6c6786fbd7b1061c7d31087d48054e1c4bed9cdc
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-15 15:50:39 +0530

    Add

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -193,7 +193,8 @@
 // ======================================================================================
 
             // find the nodes between range value 
-            
+
+import java.util.*;
 
 public class BinarySearchTree { 
 
@@ -232,6 +233,7 @@ public class BinarySearchTree {
         inorder(root.right);
     }
 
+    // ---------- printInRange---------------  
     public static void printInRange(Node root, int k1, int k2) {
         if(root == null) {
             return; 
@@ -247,7 +249,38 @@ public class BinarySearchTree {
         else{
             printInRange(root.right, k1, k2);
         }
+    } 
+
+// ===================================================================
+// =================================================================== 
+
+            // Root 2 leaf path 
+
+    public static void printPath(ArrayList<Integer> path) {
+        for(int i=0; i<path.size(); i++) {
+            System.out.print(path.get(i)+"->"); 
+        } 
+        System.out.println("N");
+    } 
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer>path){
+        if(root == null) {
+            return;
+        }
+
+        path.add(root.data); 
+        if(root.left == null && root.right == null) {
+            printPath(path); 
+        }
+
+        printRoot2Leaf(root.left, path); 
+        printRoot2Leaf(root.right, path); 
+        path.remove(path.size()-1); 
+
     }
+
+// ============================================================================= 
+// =============================================================================
 
     public static void main(String args []) {
         int values [] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
@@ -260,7 +293,30 @@ public class BinarySearchTree {
         inorder(root);
         System.out.println();
 
-        printInRange(root, 5, 12);
+        // printInRange(root, 5, 12); // for range 
+
+        printRoot2Leaf(root, new ArrayList<>()); // for 2leaf 
+
+
     }
 }
+
+
+
+// ======================================================================================
+// ======================================================================================
+
+    // root to leaf Paths :- 
+
+
+
+
+
+
+
+
+
+
+
+
 
