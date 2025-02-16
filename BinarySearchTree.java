b4227e86bc65fb3380dffa6b5be2669ac84212c1
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-16 22:14:03 +0530

    Add

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -335,89 +335,81 @@
                         // Mirror a BST :-  
 
 
-import java.util.*; 
+// import java.util.*; 
 
-public class BinarySearchTree { 
-    static class Node {
-        int data; 
-        Node right;
-        Node left; 
+// public class BinarySearchTree { 
+//     static class Node {
+//         int data; 
+//         Node right;
+//         Node left; 
 
-        Node(int data) {
-            this.data = data; 
-            this.left = this.right = null; 
+//         Node(int data) {
+//             this.data = data; 
+//             this.left = this.right = null; 
             
-        }
-    }
+//         }
+//     }
 
-    public static Node createMirror(Node root) {
-        if(root == null) {
-            return null; 
-        } 
+//     public static Node createMirror(Node root) {
+//         if(root == null) {
+//             return null; 
+//         } 
 
-        Node leftMirror = createMirror(root.left); 
-        Node rightMirror = createMirror(root.right); 
+//         Node leftMirror = createMirror(root.left); 
+//         Node rightMirror = createMirror(root.right); 
 
-        root.left = rightMirror; 
-        root.right = leftMirror; 
-        return root; 
-    } 
+//         root.left = rightMirror; 
+//         root.right = leftMirror; 
+//         return root; 
+//     } 
 
-    public static void preorder(Node root) {
-        if(root == null) {
-            return; 
+//     public static void preorder(Node root) {
+//         if(root == null) {
+//             return; 
 
-        }
+//         }
 
-        System.out.print(root.data + " "); 
-        preorder(root.left); 
-        preorder(root.right); 
-    }
+//         System.out.print(root.data + " "); 
+//         preorder(root.left); 
+//         preorder(root.right); 
+//     }
 
-    public static void main(String args[]) { 
+//     public static void main(String args[]) { 
 
-        /*
-                     8
-                    / \ 
-                   5   10
-                  / \   \
-                 3   6   11
+//         /*
+//                      8
+//                     / \ 
+//                    5   10
+//                   / \   \
+//                  3   6   11
           
-         */
-        Node root = new Node(8); 
-        root.left = new Node(5); 
-        root.right = new Node(10); 
-        root.left.left = new Node(3);
-        root.left.right = new Node(6); 
-        root.right.right = new Node(11);   
+//          */
+//         Node root = new Node(8); 
+//         root.left = new Node(5); 
+//         root.right = new Node(10); 
+//         root.left.left = new Node(3);
+//         root.left.right = new Node(6); 
+//         root.right.right = new Node(11);   
 
-        System.out.println("Original preorder");
-        preorder(root);
-        System.out.println();
+//         System.out.println("Original preorder");
+//         preorder(root);
+//         System.out.println();
  
-        /* 
-                    8
-                  /  \
-                 10   5
-                /    / \ 
-               11   6   3 
-               Mirror BST 
-         */
+//         /* 
+//                     8
+//                   /  \
+//                  10   5
+//                 /    / \ 
+//                11   6   3 
+//                Mirror BST 
+//          */
 
-        System.out.print("Mirror preorder = ");
-        root = createMirror(root); 
-        preorder(root); 
+//         System.out.print("Mirror preorder = ");
+//         root = createMirror(root); 
+//         preorder(root); 
 
-    }
-}
-
-
+//     }
+// }
 
 
 
-
-
-
-
-
-// 
