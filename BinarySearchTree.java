38cb0ab9c85dbe832d31daf274acfd7bf8a1d4ff
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-18 21:08:37 +0530

    Add

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -420,14 +420,12 @@ public class BinarySearchTree {
 
     public static void main(String args []) {
         Scanner sc = new Scanner(System.in); 
-        int n = 100; 
-        for(int i=0; i<n; i++){
-            System.out.println("devil");
-        }djfji 
+        int n = 5; 
+        for(int i=0; i<n; i++){ 
+            for(int j=1; j<= i; j++){
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
     }
-}dfjf adfj 
-
-
-iruof 
-
-djfjdf 
+}
