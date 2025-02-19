7fba2bf7230b2cbd120e59dd218e70b717c0e09f
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-19 10:12:53 +0530

    Add

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -414,18 +414,81 @@
 
 // ======================================================================================================== 
 
+
+        // sorted array to balanced BST :- 
+        
+
 import java.util.*; 
 
-public class BinarySearchTree {
+public class BinarySearchTree { 
+    static class Node {
+        int data; 
+        Node left; 
+        Node right;  
 
-    public static void main(String args []) {
-        Scanner sc = new Scanner(System.in); 
-        int n = 5; 
-        for(int i=0; i<n; i++){ 
-            for(int j=1; j<= i; j++){
-                System.out.print("*");
-            }
-            System.out.println(); 
+        public Node(int data) {
+            this.data = data; 
+            this.left = this.right = null; 
         }
+    } 
+
+    public static void preorder(Node root) {
+        if(root == null) {
+            return;  
+        } 
+
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    } 
+
+    public static Node createBST(int arr[], int st, int end) { 
+        if(st > end) {
+            return null; 
+        }
+
+        int mid = (st + end)/2; 
+        Node root = new Node(arr[mid]); 
+        root.left = createBST(arr, st, mid-1); 
+        root.right = createBST(arr, mid+1, end); 
+        return root; 
+
     }
-}dfj  pofd pojr 
+     
+
+    public static void main(String args []) { 
+        int arr[] = {3, 5, 6, 8, 10, 11, 12}; 
+
+        /*
+                       8
+                    /     \
+                   5      11
+                  / \     / \
+                 3   6   10  12
+
+         */
+
+        Node root = createBST(arr, 0, arr.length-1);
+        preorder(root);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
