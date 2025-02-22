dda05d43f201a9519f4c8916147cf8379b3c8f12
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-22 15:08:47 +0530

    Add

--- Heaps.java
+++ Heaps.java
@@ -225,52 +225,83 @@
 // =============================================================================================
 // =============================================================================================
 
- 
-import java.util.*; 
 
-public class Heaps {  
-    static class Point implements Comparable<Point> {
-        int x; 
-        int y; 
-        int distSq; 
-        int idx; 
-
-        public Point(int x, int y, int distSq, int idx) {
-            this.x = x; 
-            this.y = y; 
-            this.distSq = distSq; 
-            this.idx = idx; 
-        } 
-
-        @Override
-        public int compareTo(Point p2) {
-            return this.distSq - p2.distSq; 
-        }
-    }
-
-    public static void main(String args []) { 
-        int pts[][] = {{3,3}, {5,-1}, {-2,4}};
-        int k = 2; 
-
-        PriorityQueue<Point> pq = new PriorityQueue<>(); 
-        for(int i=0; i<pts.length; i++){
-            int distSq = pts[i][0]*pts[i][0] + pts[i][1]*pts[i][1];
-            pq.add(new Point(pts[i][0], pts[i][1], distSq, i));
-        }
-
-        // nearest k cars 
-        for(int i=0; i<k; i++){
-            System.out.println("C"+pq.remove().idx);
-        }
-    }
-}
+            // Near by cars 
 
 
+// import java.util.*; 
+
+// public class Heaps {  
+//     static class Point implements Comparable<Point> {
+//         int x; 
+//         int y; 
+//         int distSq; 
+//         int idx; 
+
+//         public Point(int x, int y, int distSq, int idx) {
+//             this.x = x; 
+//             this.y = y; 
+//             this.distSq = distSq; 
+//             this.idx = idx; 
+//         } 
+
+//         @Override
+//         public int compareTo(Point p2) {
+//             return this.distSq - p2.distSq; 
+//         }
+//     }
+
+//     public static void main(String args []) { 
+//         int pts[][] = {{3,3}, {5,-1}, {-2,4}};
+//         int k = 2; 
+
+//         PriorityQueue<Point> pq = new PriorityQueue<>(); 
+//         for(int i=0; i<pts.length; i++){
+//             int distSq = pts[i][0]*pts[i][0] + pts[i][1]*pts[i][1];
+//             pq.add(new Point(pts[i][0], pts[i][1], distSq, i));
+//         }
+
+//         // nearest k cars 
+//         for(int i=0; i<k; i++){
+//             System.out.println("C"+pq.remove().idx);
+//         }
+//     }
+// }
 
 
+// =============================================================================================
+// =============================================================================================
 
 
+            // connect n ropes to Minimum cost 
+            
+// import java.util.*; 
 
+// public class Heaps {
+
+//     public static void main(String args []) {
+//         int ropes[] = {2, 3, 3, 4, 6};
+
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+//         for(int i=0; i<ropes.length; i++ ){
+//             pq.add(ropes[i]);
+//         }
+
+//         int cost = 0; 
+//         while(pq.size() > 1) {
+//             int min = pq.remove(); 
+//             int min2 = pq.remove(); 
+//             cost += min + min2; 
+//             pq.add(min+min2); 
+//         } 
+
+//         System.out.println("cost of connecting n ropes = " + cost);
+//     }
+// }
+
+
+// =============================================================================================
+// =============================================================================================
 
 
 
