8d5d1478556a4be2923a315ca01757eaeab35a11
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-22 08:57:23 +0530

    Add

--- Heaps.java
+++ Heaps.java
@@ -70,52 +70,101 @@
 // ==============================================================================
 
 
-    // insert in heap 
+    // insert in heap and delete 
 
 
-import java.util.*; 
+// import java.util.*; 
 
-public class Heaps { 
+// public class Heaps { 
 
-    static class Heap {
-        ArrayList<Integer> arr = new ArrayList<>(); 
+//     static class Heap {
+//         ArrayList<Integer> arr = new ArrayList<>(); 
 
-        public void add(int data) {
-            // add at last idx 
-            arr.add(data); 
+//         public void add(int data) {
+//             // add at last idx 
+//             arr.add(data); 
 
-            int x = arr.size()-1; 
-            int par = (x-1)/2; 
+//             int x = arr.size()-1; 
+//             int par = (x-1)/2; 
 
-            while(arr.get(x) < arr.get(par)) {
-                // swap 
-                int temp = arr.get(x); 
-                arr.set(x, arr.get(par)); 
-                arr.set(par,temp);
-            }
-        } 
-        public int peek() {
-            return arr.get(4);
-        }
-    }
+//             while(arr.get(x) < arr.get(par)) {
+//                 // swap 
+//                 int temp = arr.get(x); 
+//                 arr.set(x, arr.get(par)); 
+//                 arr.set(par,temp); 
 
-    public static void main(String args []) {    
-        Heap heap = new Heap(); 
-        heap.add(4);
-        heap.add(1);
-        heap.add(3);
-        heap.add(9);
-        heap.add(6);
-        heap.add(5); 
+//                 x = par;
+//                 par = (x-1)/2;
+//             }
+//         } 
+//         public int peek() {
+//             return arr.get(0);
+//         } 
 
-        System.out.println("min element = " + heap.peek()); 
-        heap.printHeap(); 
+//         // heapify
+//         private void heapify (int i) {
+//             int left = 2*i+1; 
+//             int right = 2*i+2; 
+//             int minIdx = i; 
+
+//             if(left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+//                 minIdx = left; 
+//             } 
+
+//             if(right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+//                 minIdx = right; 
+//             } 
+
+//             if(minIdx != i) {
+//                 int temp = arr.get(i);
+//                 arr.set(i, arr.get(minIdx)); 
+//                 arr.set(minIdx, temp);
+
+//                 heapify(minIdx);
+//             }
+//         }  
+
+//         // remove 
+//         public int remove() {
+//             int data = arr.get(0); 
+        
+
+//             // step-1 swap first & last 
+//             int temp = arr.get(0); 
+//             arr.set(0, arr.get(arr.size()-1)); 
+//             arr.set(arr.size()-1, temp); 
+
+//             // step-2 delete last 
+//             arr.remove(arr.size()-1); 
+
+//             // step -3 heapify 
+//             heapify(0); 
+//             return data; 
+//         }
+
+//         public boolean isEmpty() {
+//             return arr.size() == 0; 
+//         }  
+//     }
+
+//     public static void main(String args []) {     
+//         Heap h = new Heap(); 
+//         h.add(3);
+//         h.add(9);
+//         h.add(1);
+//         h.add(6); 
+
+//         while(!h.isEmpty()) {
+//             System.out.println(h.peek());
+//             h.remove(); 
+//         }
+//     }
+// }
 
 
-    }
-}
+// ============================================================================================= 
+// =============================================================================================
 
-errrq r 
 
 
 
