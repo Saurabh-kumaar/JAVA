5e6c8a4dbe63b50d697cc132fee61ed39e0ae135
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-23 20:53:25 +0530

    Add

--- HashMaps.java
+++ HashMaps.java
@@ -1,38 +1,142 @@
-import java.util.*; 
-public class HashMaps {   
 
-    public static void main(String args []) {
-        // create 
-        HashMap<String, Integer> hm = new HashMap<>(); 
+//         // lets starting HashMap :- 
 
-        // Insert 
-        hm.put("Bihar", 200); 
-        hm.put("uttar pradesh", 120); 
-        hm.put("punjab", 140); 
+
+
+// import java.util.*; 
+// public class HashMaps {   
+
+//     public static void main(String args []) {
+//         // create 
+//         HashMap<String, Integer> hm = new HashMap<>(); 
+
+//         // Insert 
+//         hm.put("Bihar", 200); 
+//         hm.put("uttar pradesh", 120); 
+//         hm.put("punjab", 140); 
         
-        System.out.println(hm); 
+//         System.out.println(hm); 
 
-        // get 
-        int budget = hm.get("Bihar");
-        System.out.println(budget); 
+//         // get 
+//         int budget = hm.get("Bihar");
+//         System.out.println(budget); 
 
-        System.out.println(hm.get("pakistan"));  // for null value 
+//         System.out.println(hm.get("pakistan"));  // for null value 
 
-        // containskey 
-        System.out.println(hm.containsKey("Bihar"));
-        System.out.println(hm.containsKey("pakistan")); 
+//         // containskey 
+//         System.out.println(hm.containsKey("Bihar"));
+//         System.out.println(hm.containsKey("pakistan")); 
 
-        // remove 
-        hm.remove("Bihar");
-        System.out.println(hm); 
+//         // remove 
+//         hm.remove("Bihar");
+//         System.out.println(hm); 
 
-        // size 
-        System.out.println(hm.size());    // 2 
+//         // size 
+//         System.out.println(hm.size());    // 2 
 
-        // Is empty 
-        hm.clear();
-        System.out.println(hm.isEmpty());
+//         // Is empty 
+//         hm.clear();
+//         System.out.println(hm.isEmpty());
 
-        
-    } 
-}
+//     } 
+// }
+
+
+// =========================================================================================
+// =========================================================================================
+
+
+        // Iteration on HashMap 
+
+// import java.util.*; 
+
+// public class HashMaps {
+
+//     public static void main(String args []) {
+//         HashMap< String , Integer> hm = new HashMap<>(); 
+//         hm.put("India", 900); 
+//         hm.put("pakistan", 300); 
+//         hm.put("Nepal", 400); 
+//         hm.put("USA", 700); 
+//         hm.put("Chaina", 500); 
+
+//         // Iterate 
+//         Set<String> keys = hm.keySet(); 
+//         System.out.println(keys); 
+
+//         for (String k: keys) {
+//             System.out.println("Key = "+k +" and value = "+hm.get(k));
+            
+//         }
+//     }
+// }
+
+
+// =========================================================================================
+// =========================================================================================
+
+        // Implementaion HashMap 
+
+// import java.util.*; 
+
+
+// public class HashMaps {
+
+//     public static void main(String args []) {
+//         static class HashMap<K,V> {
+//             private  class 
+//         }
+//     }
+// }
+
+
+// =========================================================================================
+// =========================================================================================
+
+        // LinkedHashMap :- 
+
+// import java.util.*; 
+
+// public class HashMaps { 
+//     public static void main(String args[]) { 
+
+//         TreeMap<String, Integer> tm = new TreeMap<>();  // treeMap 
+//         tm.put("Mange", 100); 
+//         tm.put("Apple", 200); 
+//         tm.put("Grapes", 60); 
+
+//         HashMap<String, Integer> hm = new HashMap<>();   // 
+//         hm.put("Mange", 100); 
+//         hm.put("Apple", 200); 
+//         hm.put("Grapes", 90);  
+
+//         System.out.println("it is come by digit = " + hm);
+//         System.out.println("it is come by alphabates =" + tm);
+
+//     }
+// }
+
+
+// =========================================================================================
+// =========================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
