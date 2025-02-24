be7134a69ff11e26ae7f9662f956bf3e354d4707
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-24 21:45:47 +0530

    Add

--- HashMaps.java
+++ HashMaps.java
@@ -123,34 +123,60 @@
 
                 // MAJORITY Element :- 
 
-import java.util.*; 
+// import java.util.*; 
 
-public class HashMaps { 
+// public class HashMaps { 
 
-    public static void main(String args[]) { 
-        int arr[] = {1, 3, 2, 5, 1, 3, 1, 5, 1}; 
-        HashMap<Integer, Integer> map = new HashMap<>(); 
+//     public static void main(String args[]) { 
+//         int arr[] = {1, 3, 2, 5, 1, 3, 1, 5, 1}; 
+//         HashMap<Integer, Integer> map = new HashMap<>(); 
 
-        for(int i=0; i<arr.length; i++) {
-            if(map.containsKey(arr[i])) {
-                map.put(arr[i], map.get(arr[i]) + 1); 
-            } else {
-                map.put(arr[i], 1); 
-            }
-        } 
+//         for(int i=0; i<arr.length; i++) {
+//             if(map.containsKey(arr[i])) {
+//                 map.put(arr[i], map.get(arr[i]) + 1); 
+//             } else {
+//                 map.put(arr[i], 1); 
+//             }
+//         } 
 
-        Set<Integer> keySet = map.keySet(); 
-        for(Integer key : keySet) {
-            if(map.get(key) > arr.length/3) {
-                System.out.println(key);
-            }
-        }
+//         Set<Integer> keySet = map.keySet(); 
+//         for(Integer key : keySet) {
+//             if(map.get(key) > arr.length/3) {
+//                 System.out.println("majority elemet is = "+ key);
+//             }
+//         }
 
-    }
-}
+//     }
+// }
 
 
+// =========================================================================================
+// =========================================================================================
 
+// import java.util.*; 
+// public class HashMaps {
+
+//     public static void main(String args[]) { 
+//         int arr[] = {1,8,5,3};
+//         HashMap<Integer , Integer> hm = new HashMap<>();  
+//         int ans [] = new int[2]; 
+
+//         for(int i=0; i<nums.length; i++) {
+//             int currEle = nums[i]; 
+//             int remainingSum = target - currEle; 
+
+//             if(hm.containsKey(remainingSum)) {
+//                 ans[0] = hm.get(remainingSum); 
+//                 ans[1] = i; 
+
+//                 return ans; 
+//             }
+//             hm.put(currEle, i); 
+//         }
+//         return ans; 
+
+//     }
+// }
 
 
 
