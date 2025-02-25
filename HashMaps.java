e9bad56d08b9ef922cd3370e3e1b3807590ca81f
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-02-25 12:25:41 +0530

    Add

--- HashMaps.java
+++ HashMaps.java
@@ -180,6 +180,58 @@
 
 
 
+// Leetcode :- 560 
+
+// subarray sum equal k 
+
+
+// class Solution { 
+//     public int subarraySum(vector<int> & nums ,int k ) {
+//         int n = nums.size; 
+//         int count = 0; 
+
+//         for(int i=0; i<n; i++){
+//             sum = 0;  
+//             for(int j=i; j<n; j++){
+//                 sum += nums[j]; 
+//                 if(sum == k) count++; 
+//             }
+//         } 
+//         return count; 
+//     } 
+// } 
+ 
+
+
+import java.util.*;
+
+public class HashMaps {  
+
+    public int subarraySum(int nums[] , int k) {
+        int n = nums.length; 
+        int count = 0; 
+
+        for(int i=0; i<n; i++) {
+            int sum = 0; 
+
+            for(int j=i; j<n; j++) {
+                sum += nums[j];  
+
+                if( sum == k) count++; 
+
+            }
+        } 
+        return count++; 
+    }
+
+
+    public static void main(String args[]) {
+        HashMaps sol = new HashMaps(); 
+        int nums [] = {1, 2, 3,3};
+        int k = 3; 
+        System.out.println(sol.subarraySum(nums, k));
+    }
+}
 
 
 
@@ -189,16 +241,5 @@
 
 
 
-dfjjkf 
 
-
-
-
-
-dff 
-
-
-
-
-dff 
 
