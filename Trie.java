dde0d98f210946d2b85ec1a255795b19f1c2823a
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-03-01 17:21:43 +0530

    Add

--- Trie.java
+++ Trie.java
@@ -1,75 +1,145 @@
-import java.util.*; 
-public class Trie {   
-    static class Node {
-        Node children[] = new Node[26];
-        boolean eow = false;
 
-        Node() {
-            for(int i=0; i<26; i++) {
+//         // we started trie
+
+
+// import java.util.*; 
+// public class Trie {   
+//     static class Node {
+//         Node children[] = new Node[26];
+//         boolean eow = false;
+
+//         Node() {
+//             for(int i=0; i<26; i++) {
+//                 children[i] = null; 
+//             }
+//         }   
+//     } 
+
+//     public static Node root = new Node();
+
+//     // insert 
+//     public static void insert(String word) {
+//         Node curr = root; 
+//         for(int level=0; level<word.length(); level++) {
+//             int idx = word.charAt(level) - 'a'; 
+//             if(curr.children[idx] == null) {
+//                 curr.children[idx] = new Node(); 
+//             } 
+//             curr = curr.children[idx]; 
+//         }
+//         curr.eow = true; 
+//     } 
+
+//     // search 
+//     public static boolean search(String key) {
+//         Node curr = root; 
+//         for(int level=0; level<key.length(); level++) {
+//             int idx = key.charAt(level)-'a'; 
+//             if(curr.children[idx] == null) {
+//                 return false; 
+//             } 
+//             curr = curr.children[idx]; 
+//         } 
+//         return curr.eow == true; 
+//     } 
+
+//     // wordBreak 
+//     public static boolean wordBreak(String key) { 
+//         if(key.length() == 0) {
+//             return true; 
+//         }
+
+//         for(int i=1; i<=key.length(); i++) {
+//             if(search(key.substring(0,i)) && 
+//             wordBreak(key.substring(i))) {
+//                 return true; 
+//             }
+//         }
+//         return false; 
+//     }
+
+
+
+//     public static void main(String args[]) {
+//         // String words[] = {"the", "a", "there", "their", "any", "thee"};
+//         // for(int i=0; i<words.length; i++) {
+//         //     insert(words[i]); 
+//         // }
+//         // System.out.println(search("thee"));
+//         // System.out.println(search("thorr")); 
+
+//         String arr[] = {"i", "like", "sam", "samsung", "mobile", "ice"}; 
+//         for(int i=0; i<arr.length; i++) {
+//             insert(arr[i]); 
+//         }
+        
+//         String key = "ilike"; 
+//         System.out.println(wordBreak(key));
+//     }
+// }
+
+
+// ================================================================================================== 
+// ================================================================================================== 
+
+import java.util.*; 
+
+public class Trie {  
+    static class Node {
+        boolean eow = false; 
+        int freq; 
+        Node children[] = new Node[26];
+
+        public Node() {
+            for(int i=0; i<children.length; i++) {
                 children[i] = null; 
-            }
-        }   
+            } 
+            freq = 1;
+        }
     } 
 
-    public static Node root = new Node();
+    public static Node root = new Node(); 
 
-    // insert 
     public static void insert(String word) {
         Node curr = root; 
-        for(int level=0; level<word.length(); level++) {
-            int idx = word.charAt(level) - 'a'; 
+        for(int i=0; i<word.length(); i++) {
+            int idx = word.charAt(i)-'a';
             if(curr.children[idx] == null) {
                 curr.children[idx] = new Node(); 
+            } else {
+                curr.children[idx].freq++; 
             } 
-            curr = curr.children[idx]; 
+
+            curr = curr.children[idx];
         }
         curr.eow = true; 
     } 
 
-    // search 
-    public static boolean search(String key) {
-        Node curr = root; 
-        for(int level=0; level<key.length(); level++) {
-            int idx = key.charAt(level)-'a'; 
-            if(curr.children[idx] == null) {
-                return false; 
-            } 
-            curr = curr.children[idx]; 
+    public static void findPrefix(Node root, String ans) {
+        if(root == null){
+            return; 
         } 
-        return curr.eow == true; 
-    } 
 
-    // wordBreak 
-    public static boolean wordBreak(String key) { 
-        if(key.length() == 0) {
-            return true; 
-        }
+        if(root.freq == 1) {
+            System.out.println(ans); 
+            return; 
+        } 
 
-        for(int i=1; i<=key.length(); i++) {
-            if(search(key.substring(0,i)) && 
-            wordBreak(key.substring(i))) {
-                return true; 
+        for(int i=0; i<root.children.length; i++) {
+            if(root.children[i] != null) {
+                findPrefix(root.children[i],ans+(char)(i+'a'));
             }
         }
-        return false; 
     }
-
     
-
-    public static void main(String args[]) {
-        // String words[] = {"the", "a", "there", "their", "any", "thee"};
-        // for(int i=0; i<words.length; i++) {
-        //     insert(words[i]); 
-        // }
-        // System.out.println(search("thee"));
-        // System.out.println(search("thorr")); 
-
-        String arr[] = {"i", "like", "sam", "samsung", "mobile", "ice"}; 
+    public static void main(String args[]) {  
+        String arr[] = {"Zebra", "dog", "duck", "dove"}; 
         for(int i=0; i<arr.length; i++) {
-            insert(arr[i]); 
+            insert(arr[i].toLowerCase());
         }
-        
-        String key = "ilike"; 
-        System.out.println(wordBreak(key));
-    }
+        root.freq = -1;
+        findPrefix(root, "");
+      }
 }
+
+
