52ffb17cd69046ab2e261e28a836dd6012bb6fd1
Saurabh-kumaar <kumarsaurabh59290@gmail.com>
2025-03-04 08:56:09 +0530

    Add

--- GRAPH/Graph1.java
+++ GRAPH/Graph1.java
@@ -143,25 +143,25 @@
 //     }  
 
 //     public static void main(String args []) {
-//         /*
-//               1 ---- 3 
-//              /       | \
-//             0        |  5 -- 6 
-//             \        | / 
-//              2 ----- 4 
+        // /*
+        //       1 ---- 3 
+        //      /       | \
+        //     0        |  5 -- 6 
+        //     \        | / 
+        //      2 ----- 4 
 
-//          */ 
-//         int V = 7; 
-//         ArrayList<Edge> graph[] = new ArrayList[V]; 
-//         createGraph(graph);
-//         bfs(graph); 
+        //  */ 
+        // int V = 7; 
+        // ArrayList<Edge> graph[] = new ArrayList[V]; 
+        // createGraph(graph);
+        // bfs(graph); 
 //     }
 
 // }
 
 // =======================================================
 
-        // DFS(DEPTH FIRST SEARCH) 
+        DFS(DEPTH FIRST SEARCH) 
 
 
 import java.util.*;  
